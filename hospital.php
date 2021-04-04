b4d50381b4a5dea8cf6339c6dc5ff45ce590d40a
tarosss <fdbeesk@gmail.com>
2021-04-04 20:30:01 +0900

    hospital

---
+++ hospital.php
@@ -0,0 +1,244 @@
+{function loop}
+<!DOCTYPE html> 
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>加藤病院</title>
+        <link rel="stylesheet" href="hospital.css?<?php echo date('Ymd-Hi'); ?>" type="text/css">   
+     </head>
+    <body>
+        <section class="header">
+            <div class="header-left">
+                <img src="header-logo.svg" class="header-left-logo">
+                <img src="logo_dept.svg" class="header-left-logo_dept"></p>
+            </div>
+            <div class="header-right">
+                    <a href="" class="header-right-btn-reservation"><p class="reservation-text"> WEB予約はこちら</p></a>
+                    <a href="" class="header-right-btn-online"><p class="online-text">エコー動画オンライン閲覧</p></a>
+            </div>
+        </section>
+
+        
+        <nav class="tagtable">
+            
+            <ul class="tags">
+                <li><a href="">HOME</a></li>
+
+                <li><p>診察案内</p>
+                    <ul>
+                        <li><p>産科</p></li>
+                        <li><p>婦人科</p></li>
+                        <li><p>内科</p></li>
+                        <li><p>小児科</p></li>
+                        <li><p>栄養外来</p></li>
+                        <li><p>美容外来</p></li>
+                        <li><p>助産師外来</p></li>
+                        <li><p>マザークラス</p></li>
+                    </ul>
+                </li>
+                <li><a href="">入院</a></li>
+                <li><p>出産のご案内</p>
+                    <ul>
+                        <li><p>当院の出産</p></li>
+                        <li><p>無痛分娩</p></li>
+                    </ul>
+                </li>
+                <li><p>病院紹介</p>
+                    <ul>
+                        <li><p>医師紹介</p></li>
+                        <li><p>施設のご案内</p></li>
+                    </ul>
+                </li>
+                <li><a>よくあるご質問</a></li>
+                <li><a>診察時間・アクセス</a></li>
+            </ul>
+        </nav>
+
+        <div class="hospital-pic">
+            <img src="hospital.jpeg" class="hospital-picture">
+        </div>
+
+        <div class="hospital-lead">
+            <p class="hospital-lead-text">加藤病院が患者様にご提供するのは<sapn class="hospital-lead-text-span">安心と安全の医療</sapn>。<br>
+            緊急の場合も、<span class="hospital-lead-text-span">複数の医師が対応する体制</span>を整えています。</p>
+        </div>
+
+        <section class="contents">
+            <div class="contents-infomation">
+                <div class="infomation">おしらせ<br>
+                    <span class="infomation-span">Infomation</span>
+                </div>
+  
+                <div class="notice-frame">
+                    <hr>
+                    <div class="notice-list">
+                        
+                        <ul>
+                            <li class="notice-list-item"><p class="notice-icon">2021-3-21</p><p class="notice-text">テキストテキストテキスト</p></li>
+                            <li class="notice-list-item"><p class="notice-icon">2021-3-21</p><p class="notice-text">テキストテキストテキスト</p></li>
+                            <li class="notice-list-item"><p class="notice-icon">2021-3-21</p><p class="notice-text">テキストテキストテキスト</p></li>
+                            <li class="notice-list-item"><p class="notice-icon">2021-3-21</p><p class="notice-text">テキストテキストテキスト</p></li>
+                            <li class="notice-list-item"><p class="notice-icon">2021-3-21</p><p class="notice-text">テキストテキストテキスト</p></li>
+                            <li class="notice-list-item"><p class="notice-icon">2021-3-21</p><p class="notice-text">テキストテキストテキスト</p></li>
+                            <li class="notice-list-item"><p class="notice-icon">2021-3-21</p><p class="notice-text">テキストテキストテキスト</p></li>
+                        </ul>
+                    </div>
+                    <hr>
+                    <div class="doctorcharge">
+                        <div class="doctor-heading">
+                            <p class="doctor-heading-text">担当医予定表</p>                 
+                        </div>
+                        <div class="doctor-table-frame">
+                            <table class="doctor-table" border="1">
+                                <thead>
+                                    <tr>
+                                        <th>曜日</th>
+                                        <th>婦人科</th>
+                                        <th>産科・小児科</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td class="table-text">本体 1</td>
+                                        <td class="table-text">本体 2</td>
+                                        <td class="table-text">本体 3</td>
+                                    </tr>
+                                </tbody>
+                                <tbody>
+                                    <tr>
+                                        <td class="table-text">本体 1</td>
+                                        <td class="table-text">本体 2</td>
+                                        <td class="table-text">本体 3</td>
+                                    </tr>
+                                </tbody>
+                                <tbody>
+                                    <tr>
+                                        <td class="table-text">本体 1</td>
+                                        <td class="table-text">本体 2</td>
+                                        <td class="table-text">本体 3</td>
+                                    </tr>
+                                </tbody>
+                                <tbody>
+                                    <tr>
+                                        <td class="table-text">本体 1</td>
+                                        <td class="table-text">本体 2</td>
+                                        <td class="table-text">本体 3</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <div class="doctor-table-text">
+                                <p>※都合により変更する場合があります。ご了承ください。</p>
+                                <p> ※小児科は日曜日休診となります。</p>
+                                <p> ※氏名併記の欄については、いずれか1名が担当します。</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="medicalinfomation">
+            <div class="medicalinfomation-heading">
+                <p>診察案内</p>
+                <p class="medicalinfomation-heading-span">Menu</p>
+            </div>
+            <div class="medicalinfomation-images">
+                <div class="medicalinfomation-image">
+                    <img src="leaf.png">
+                    <P>産科</P>
+                </div>
+                <div class="medicalinfomation-image">
+                    <img src="leaf.png">
+                    <P>婦人科</P>
+                </div>                <div class="medicalinfomation-image">
+                    <img src="leaf.png">
+                    <P>内科</P>
+                </div>                <div class="medicalinfomation-image">
+                    <img src="leaf.png">
+                    <P>小児科</P>
+                </div>                <div class="medicalinfomation-image">
+                    <img src="leaf.png">
+                    <P>美容外来</P>
+                </div>
+            </div>
+        </section>
+
+        <section class="featureinformation">
+            <div class="featureinformation-header">
+                <p>当院の特徴</p>
+                <span class="lko">Feature</span>
+                <p></p>
+            </div>
+            <div class="allfeatures">
+                <div class="leftfeatures">
+                    <div class="feature-item">
+                        <img src="dog.jpeg" class="feature-image">
+                        <div>
+                            <p class="feature-item-spantext">トータルの医療体制</p>
+                            <p class="feature-item-text">医師、薬剤師、助産師、<br>
+                                看護師、管理栄養士による<br>
+                                トータルな医療体制
+                            </p>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="feature-item">
+                        <img src="dog.jpeg" class="feature-image">
+                        <div>
+                            <p class="feature-item-spantext">トータルの医療体制</p>
+                            <p class="feature-item-text">医師、薬剤師、助産師、<br>
+                                看護師、管理栄養士による<br>
+                                トータルな医療体制
+                            </p>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="feature-item">
+                        <img src="dog.jpeg" class="feature-image">
+                        <div>
+                            <p class="feature-item-spantext">トータルの医療体制</p>
+                            <p class="feature-item-text">医師、薬剤師、助産師、<br>
+                                看護師、管理栄養士による<br>
+                                トータルな医療体制
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="rightfeatures">
+                    <div class="feature-item">
+                        <img src="dog.jpeg" class="feature-image">
+                        <div>
+                            <p class="feature-item-spantext">トータルの医療体制</p>
+                            <p class="feature-item-text">医師、薬剤師、助産師、<br>
+                                看護師、管理栄養士による<br>
+                                トータルな医療体制
+                            </p>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="feature-item">
+                        <img src="dog.jpeg" class="feature-image">
+                        <div>
+                            <p class="feature-item-spantext">トータルの医療体制</p>
+                            <p class="feature-item-text">医師、薬剤師、助産師、<br>
+                                看護師、管理栄養士による<br>
+                                トータルな医療体制
+                            </p>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="feature-item">
+                        <img src="dog.jpeg" class="feature-image">
+                        <div>
+                            <p class="feature-item-spantext">トータルの医療体制</p>
+                            <p class="feature-item-text">医師、薬剤師、助産師、<br>
+                                看護師、管理栄養士による<br>
+                                トータルな医療体制
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    <script src="hospital.js"></script>
+    </body>
+</html>
